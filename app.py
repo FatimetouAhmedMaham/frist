801398096bb9fbf605fd229b52807df9e9c1297d
deide <deidemedlemon@gmail.com>
2025-01-28 16:59:27 +0000

    test build

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
 # app.py
 from flask import Flask, jsonify
-import datetime
+# import datetime
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({
-        "message": "Bienvenue sur l'API ",
-        "timestamp": datetime.datetime.now().isoformat()
-    })
+    return "hello deide"
 
-@app.route('/health ')
-def health():
-    return jsonify({
-        " status ": " healthy ",
-        " version ": " 1.0.0 "
-    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
